2aafc3138639a999e1b727f03f393f7326200e7a
mohammad pias <mrpeyash@gmail.com>
2022-06-05 19:07:32 +0600

    server updated

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 var admin = require("firebase-admin");
+const SSLCommerzPayment = require('sslcommerz')
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -104,11 +105,11 @@ async function run() {
                 }
             }
             const search = req.query.search;
-            console.log(search)
+            // console.log(search)
 
             let result = [];
             if (search === 'admins') {
-                console.log(currPage)
+                console.log(currPage, 'admin filter')
                 const cursor = userCollection.find(query);
                 result = await cursor.skip(currPage * userPerPage).limit(userPerPage).toArray()
             }
@@ -134,38 +135,76 @@ async function run() {
             res.json(result)
         })
 
+        // check admin
+        app.get('/checkAmin/:email', async (req, res) => {
+            const email = req.params.email;
+            // console.log(email)
+            const query = { email: email }
+            const result = await userCollection.findOne(query);
+
+            let admin = false;
+            if (result?.role === 'admin') {
+                admin = true
+            }
+            else {
+                admin = false
+            }
+            res.json({ admin: admin })
+        })
+
         // Products Managements ==============
 
         // add product
         app.post('/products', async (req, res) => {
             const product = req.body;
-            const result = await productCollection.insertOne(product);
-            console.log('hit the post', product, result)
+            const date = new Date().toLocaleString();
+            const time = new Date().getTime();
+            const tempProduct = { ...product, createdAtDate: date, createdAtTime: time }
+            const result = await productCollection.insertOne(tempProduct);
+            // console.log('hit the post', product, result)
             res.json(result);
         });
 
         // get all product
         app.get('/products', async (req, res) => {
             const currPage = parseInt(req.query.currPage);
-            const userPerPage = parseInt(req.query.userPerPage);
+            const productPerPage = parseInt(req.query.productPerPage);
             const filterType = req.query.filterType;
             const cursor = productCollection.find({});
             const productCount = await productCollection.estimatedDocumentCount();
-            console.log(filterType, currPage, userPerPage)
+
+            const basketProducts = await productCollection.find({ category: 'Basket' }).toArray();
+            const footwearProducts = await productCollection.find({ category: 'Footwear' }).toArray();
+
 
             let result = []
-            if (filterType === 'basket') {
-                const query = { category: 'Basket' };
-                const cursor = productCollection.find(query);
-                result = await cursor.skip(currPage * userPerPage).limit(userPerPage).toArray();
-                console.log('hitting from basket')
+            if (filterType) {
+                if (filterType === 'basket') {
+                    const query = { category: 'Basket' };
+                    const cursor = productCollection.find(query);
+                    result = await cursor.skip(currPage * productPerPage).limit(productPerPage).toArray();
+                    // console.log('hitting from basket')
+                    console.log('hitting from filter basket', currPage, productPerPage)
+                }
+                else if (filterType === 'footwear') {
+                    const cursor = productCollection.find({ category: 'Footwear' });
+                    result = await cursor.skip(currPage * productPerPage).limit(productPerPage).toArray();
+                    console.log('hitting from filter footwear')
+
+                }
+                else {
+                    result = await cursor.skip(currPage * productPerPage).limit(productPerPage).toArray();
+                    console.log('hitting from filter all product')
+                }
             }
             else {
-                result = await cursor.skip(currPage * userPerPage).limit(userPerPage).toArray();
-                console.log('hitting from all product')
+                result = await cursor.toArray();
+                console.log('hit from without filter')
             }
             res.send({
                 count: productCount,
+                basketCount: basketProducts.length,
+                footwearCount: footwearProducts.length,
                 products: result,
             })
         });
@@ -179,43 +218,201 @@ async function run() {
             res.json(result)
         })
 
-        app.post('/products/keys', async (req, res) => {
-            const arr = req.body.productKeys;
-            const newArray = []
-            if (arr?.length > 0) {
-                for (const key of arr) {
-                    newArray.push(ObjectId(key))
-                }
-            }
-            console.log('new array', newArray)
-            const query = { _id: { $in: newArray } }
-            let result = []
-            if (newArray?.length > 0) {
-                console.log('hot from the for loop')
-                result = await productCollection.find(query).toArray();
-            }
-            console.log(result, arr)
-            res.json(result)
-        })
-
-        // payment method
+        // Stripe Payment Gateway========
         app.post("/create-payment-intent", async (req, res) => {
             const { amount } = req.body;
-            console.log(stripe)
-            console.log(typeof (amount))
-            // Create a PaymentIntent with the order amount and currency
             const paymentIntent = await stripe.paymentIntents.create({
                 amount: amount * 100,
                 currency: "usd",
                 payment_method_types: ['card']
             });
-            console.log(paymentIntent.client_secret)
             res.send({
                 clientSecret: paymentIntent.client_secret,
             });
         });
 
+        // SSLCommerz Payment Gateway==========
+
+
+        app.get('/init', (req, res) => {
+            const data = {
+                total_amount: 100,
+                currency: 'EUR',
+                tran_id: 'REF123',
+                success_url: 'http://localhost:5000/success',
+                fail_url: 'http://localhost:5000/fail',
+                cancel_url: 'http://localhost:5000/cancel',
+                ipn_url: 'http://yoursite.com/ipn',
+                shipping_method: 'Courier',
+                product_name: 'Computer.',
+                product_category: 'Electronic',
+                product_profile: 'general',
+                cus_name: 'Customer Name',
+                cus_email: 'cust@yahoo.com',
+                cus_add1: 'Dhaka',
+                cus_add2: 'Dhaka',
+                cus_city: 'Dhaka',
+                cus_state: 'Dhaka',
+                cus_postcode: '1000',
+                cus_country: 'Bangladesh',
+                cus_phone: '01711111111',
+                cus_fax: '01711111111',
+                ship_name: 'Customer Name',
+                ship_add1: 'Dhaka',
+                ship_add2: 'Dhaka',
+                ship_city: 'Dhaka',
+                ship_state: 'Dhaka',
+                ship_postcode: 1000,
+                ship_country: 'Bangladesh',
+                multi_card_name: 'mastercard',
+                value_a: 'ref001_A',
+                value_b: 'ref002_B',
+                value_c: 'ref003_C',
+                value_d: 'ref004_D'
+            };
+            const sslcommer = new SSLCommerzPayment(process.env.SSL_STORE_ID, process.env.SSL_STORE_PASS, false) //true for live default false for sandbox
+            sslcommer.init(data).then(data => {
+                //process the response that got from sslcommerz 
+                //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+                res.redirect(data.GatewayPageURL)
+            });
+        })
+
+        app.post('/success', async (req, res) => {
+            console.log(req.body)
+            res.status(200).json(req.body)
+        })
+        app.post('/fail', async (req, res) => {
+            console.log(req.body)
+            res.status(200).json(req.body)
+        })
+        app.post('/cancel', async (req, res) => {
+            console.log(req.body)
+            res.status(200).json(req.body)
+        })
+
         // order management
+
+        app.post('/orders', async (req, res) => {
+            const orders = req.body;
+            const date = new Date().toLocaleString();
+            const time = new Date().getTime();
+            const tempOrder = { ...orders, createdAtDate: date, createdAtTime: time }
+            const result = await orderCollection.insertOne(tempOrder)
+            res.json(result)
+        })
+
+        // get orders
+        app.get('/myOrders', async (req, res) => {
+
+            const currPage = parseInt(req.query.currPage);
+            const orderPerPage = parseInt(req.query.orderPerPage);
+            const filterType = req.query.filterType;
+            const email = req.query.email;
+
+            const cursor = orderCollection.find({ 'shipping.email': email });
+            const totalOrders = await cursor.toArray();
+            const deliveredOrders = orderCollection.find({ 'shipping.email': email, status: 'delivered' });
+            const deliveredArray = await deliveredOrders.toArray();
+
+            console.log()
+            // let deliverOrder = [];
+            let restOrders = [];
+            let result = [];
+            if (filterType === 'delivered') {
+                const deliveredOrders = orderCollection.find({ 'shipping.email': email, status: 'delivered' });
+                result = await deliveredOrders.skip(currPage * orderPerPage).limit(orderPerPage).toArray();
+            }
+            else {
+
+                // result = await orderCollection.find({ 'shipping.email': email }).skip(currPage * orderPerPage).limit(orderPerPage).toArray();
+                if (totalOrders?.length > 0) {
+                    result = totalOrders?.filter(item => item.status !== 'delivered')
+                    // result = [...restOrders.skip(currPage * orderPerPage).limit(orderPerPage)]
+                }
+            }
+
+
+            console.log(result)
+
+            res.send({
+                totalOrders: totalOrders?.length,
+                deliveredOrders: deliveredArray?.length,
+                data: result
+            })
+        })
+        app.get('/orders', async (req, res) => {
+            const currPage = parseInt(req.query.currPage);
+            const orderPerPage = parseInt(req.query.orderPerPage);
+            const filterType = req.query.filterType;
+
+            const totalOrders = await orderCollection.estimatedDocumentCount();
+            const deliveredOrders = orderCollection.find({ status: 'delivered' });
+            const deliveredArray = await deliveredOrders.toArray()
+            const updatedOrders = orderCollection.find({ status: 'updated' });
+            const updatedArray = await updatedOrders.toArray()
+            const processingOrders = orderCollection.find({ status: 'processing' });
+            const processingArray = await processingOrders.toArray()
+
+            // console.log(currPage, orderPerPage, filterType)
+
+            let result = []
+
+            if (filterType) {
+                if (filterType === 'delivered') {
+                    const deliveredOrders = orderCollection.find({ status: 'delivered' });
+                    result = await deliveredOrders.skip(currPage * orderPerPage).limit(orderPerPage).toArray();
+                    // console.log('hit from delivered', result)
+                }
+                else if (filterType === 'updated') {
+                    const updatedOrders = orderCollection.find({ status: 'updated' });
+                    result = await updatedOrders.skip(currPage * orderPerPage).limit(orderPerPage).toArray();
+                    // console.log('hit from updated', result)
+                }
+                else if (filterType === 'processing') {
+                    const processingOrders = orderCollection.find({ status: 'processing' });
+                    result = await processingOrders.skip(currPage * orderPerPage).limit(orderPerPage).toArray();
+                    // console.log('hit from processing', result)
+                }
+                else {
+
+                    result = await orderCollection.find({}).skip(currPage * orderPerPage).limit(orderPerPage).toArray();
+                    // console.log('hit from all orders')
+                }
+            }
+            // console.log('all orders', result)
+
+            res.send({
+                totalOrders: totalOrders,
+                deliveredOrders: deliveredArray?.length,
+                updatedOrders: updatedArray?.length,
+                processingOrders: processingArray?.length,
+                data: result,
+            })
+        });
+
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await orderCollection.deleteOne(filter);
+            console.log(result);
+            res.json(result)
+        })
+
+        // update status
+        app.put('/orders', async (req, res) => {
+            const id = req.query.id;
+            const action = req.query.action;
+            const date = new Date().toLocaleString();
+            const time = new Date().getTime();
+            const filter = { _id: ObjectId(id) }
+            const updateDoc = {
+                $set: { status: action, updatedAtTime: time, updatedAtDate: date }
+            }
+            const result = await orderCollection.updateOne(filter, updateDoc);
+            console.log(result);
+            res.json(result)
+        })
 
 
     }
